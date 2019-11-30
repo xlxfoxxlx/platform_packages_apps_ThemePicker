4623ab4e13f1870152dab6b5ed421154d02d638d
shagbag913 <sh4gbag913@gmail.com>
2019-11-30 10:09:17 -0500

    Override WallpaperPicker
     * Since ThemePicker includes WallpaperPicker2, it has the same
       functionality but with a better UI as the ugly WallpaperPicker.
    
    Change-Id: I37c9ca8aefbd619f6d5a929073a838f8fbb55a7a

--- Android.mk
+++ Android.mk
@@ -32,7 +32,7 @@ LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PACKAGE_NAME := ThemePicker
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
-LOCAL_OVERRIDES_PACKAGES := WallpaperPicker2
+LOCAL_OVERRIDES_PACKAGES := WallpaperPicker WallpaperPicker2
 
 ifneq (,$(wildcard frameworks/base))
   LOCAL_PRIVATE_PLATFORM_APIS := true

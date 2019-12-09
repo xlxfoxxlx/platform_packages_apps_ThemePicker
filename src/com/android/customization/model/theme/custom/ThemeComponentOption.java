3e77c14fb13574114a5ca903ac26441962f2fc4c
El Dainosor <eldainosor@gmail.com>
2019-12-09 07:13:33 +0100

    ThemePicker: Show items name under the title
    When tapping on options, the name will show up, for all items, from fonts, icons, shapes and accent color.
    Useful for the font combos, and also our funny accent names.
    
    Change-Id: I834dbf30301dba47b1a7e850940af32e92c14b2d

--- src/com/android/customization/model/theme/custom/ThemeComponentOption.java
+++ src/com/android/customization/model/theme/custom/ThemeComponentOption.java
@@ -145,6 +145,8 @@ public abstract class ThemeComponentOption implements CustomizationOption<ThemeC
             }
             TextView title = container.findViewById(R.id.font_card_title);
             title.setTypeface(mHeadlineFont);
+            TextView header = container.findViewById(R.id.theme_preview_card_header);
+            header.setText(container.getContext().getString(R.string.preview_name_font) + "\n\u0028" + mLabel + "\u0029");
             TextView bodyText = container.findViewById(R.id.font_card_body);
             bodyText.setTypeface(mBodyFont);
             container.findViewById(R.id.font_card_divider).setBackgroundColor(
@@ -223,6 +225,8 @@ public abstract class ThemeComponentOption implements CustomizationOption<ThemeC
         public void bindPreview(ViewGroup container) {
             bindPreviewHeader(container, R.string.preview_name_icon, R.drawable.ic_wifi_24px);
 
+            TextView header = container.findViewById(R.id.theme_preview_card_header);
+            header.setText(container.getContext().getString(R.string.preview_name_icon) + "\n\u0028" + mLabel + "\u0029");
             ViewGroup cardBody = container.findViewById(R.id.theme_preview_card_body_container);
             if (cardBody.getChildCount() == 0) {
                 LayoutInflater.from(container.getContext()).inflate(
@@ -340,6 +344,8 @@ public abstract class ThemeComponentOption implements CustomizationOption<ThemeC
         public void bindPreview(ViewGroup container) {
             bindPreviewHeader(container, R.string.preview_name_color, R.drawable.ic_colorize_24px);
 
+            TextView header = container.findViewById(R.id.theme_preview_card_header);
+            header.setText(container.getContext().getString(R.string.preview_name_color) + "\n\u0028" + mLabel + "\u0029");
             ViewGroup cardBody = container.findViewById(R.id.theme_preview_card_body_container);
             if (cardBody.getChildCount() == 0) {
                 LayoutInflater.from(container.getContext()).inflate(
@@ -478,6 +484,8 @@ public abstract class ThemeComponentOption implements CustomizationOption<ThemeC
         public void bindPreview(ViewGroup container) {
             bindPreviewHeader(container, R.string.preview_name_shape, R.drawable.ic_shapes_24px);
 
+            TextView header = container.findViewById(R.id.theme_preview_card_header);
+            header.setText(container.getContext().getString(R.string.preview_name_shape) + "\n\u0028" + mLabel + "\u0029");
             ViewGroup cardBody = container.findViewById(R.id.theme_preview_card_body_container);
             if (cardBody.getChildCount() == 0) {
                 LayoutInflater.from(container.getContext()).inflate(

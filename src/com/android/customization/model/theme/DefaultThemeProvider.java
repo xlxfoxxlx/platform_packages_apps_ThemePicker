f7a9b12fd32664c57926e663060509f2a65c9b00
maxwen <max.weninger@gmail.com>
2019-12-20 16:00:23 +0100

    ThemePicker: add support for primary overlays
    and fix some themeing issues
    
    primary overlays MUST include a meta color
    primary_device_default in BOTH values and values-night
    depending if the primary overlay is for dark or light
    mode the not used value must be set to the system default
    color
    
    e.g. for a light only overlay values-night/colors.xml must have
    <color name="primary_device_default">@*android:color/primary_device_default_settings</color>
    vice versa a dark overlay in values/colors.xml
    <color name="primary_device_default">@*android:color/primary_device_default_settings_light</color>
    
    Change-Id: I9ba699188e66f1b791e3b47fa2b861b5e85b342e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/customization/model/theme/DefaultThemeProvider.java
+++ src/com/android/customization/model/theme/DefaultThemeProvider.java
@@ -25,6 +25,7 @@ import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY
 import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY_ICON_LAUNCHER;
 import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY_ICON_SETTINGS;
 import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY_ICON_SYSUI;
+import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY_PRIMARY;
 import static com.android.customization.model.ResourceConstants.OVERLAY_CATEGORY_SHAPE;
 import static com.android.customization.model.ResourceConstants.SYSUI_PACKAGE;
 
@@ -76,6 +77,7 @@ public class DefaultThemeProvider extends ResourcesApkProvider implements ThemeB
     private static final String TITLE_PREFIX = "theme_title_";
     private static final String FONT_PREFIX = "theme_overlay_font_";
     private static final String COLOR_PREFIX = "theme_overlay_color_";
+    private static final String PRIMARY_PREFIX = "theme_overlay_primary_";
     private static final String SHAPE_PREFIX = "theme_overlay_shape_";
     private static final String ICON_ANDROID_PREFIX = "theme_overlay_icon_android_";
     private static final String ICON_LAUNCHER_PREFIX = "theme_overlay_icon_launcher_";
@@ -148,6 +150,14 @@ public class DefaultThemeProvider extends ResourcesApkProvider implements ThemeB
             } catch (NameNotFoundException | NotFoundException e) {
                 Log.d(TAG, "Didn't find color overlay for theme, will use system default");
                 mOverlayProvider.addSystemDefaultColor(builder);
+            }
+
+            try {
+                String primaryOverlayPackage = getOverlayPackage(PRIMARY_PREFIX, themeName);
+                mOverlayProvider.addPrimaryOverlay(builder, primaryOverlayPackage);
+            } catch (NameNotFoundException | NotFoundException e) {
+                Log.d(TAG, "Didn't find primary overlay for theme, will use system default");
+                mOverlayProvider.addSystemDefaultPrimary(builder);
             }
 
             try {
@@ -300,6 +310,13 @@ public class DefaultThemeProvider extends ResourcesApkProvider implements ThemeB
             mOverlayProvider.addSystemDefaultColor(builder);
         }
 
+        try {
+            String primaryOverlayPackage = getOverlayPackage(PRIMARY_PREFIX, DEFAULT_THEME_NAME);
+            mOverlayProvider.addPrimaryOverlay(builder, primaryOverlayPackage);
+        } catch (NameNotFoundException | NotFoundException e) {
+            Log.d(TAG, "Didn't find color overlay for default theme, will use system default");
+            mOverlayProvider.addSystemDefaultPrimary(builder);
+        }
 
         try {
             String fontOverlayPackage = getOverlayPackage(FONT_PREFIX, DEFAULT_THEME_NAME);
@@ -464,6 +481,8 @@ public class DefaultThemeProvider extends ResourcesApkProvider implements ThemeB
                     customPackages.get(OVERLAY_CATEGORY_FONT));
             mOverlayProvider.addColorOverlay(builder,
                     customPackages.get(OVERLAY_CATEGORY_COLOR));
+            mOverlayProvider.addPrimaryOverlay(builder,
+                    customPackages.get(OVERLAY_CATEGORY_PRIMARY));
             mOverlayProvider.addAndroidIconOverlay(builder,
                     customPackages.get(OVERLAY_CATEGORY_ICON_ANDROID));
             mOverlayProvider.addSysUiIconOverlay(builder,
